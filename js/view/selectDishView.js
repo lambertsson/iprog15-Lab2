248fa6ebaaddefd14f7bb0a54daca31df1138c88
Caroline Arkenson <arkenson@kth.se>
2015-02-10 14:06:13 +0100

    started of selectDishView. added displayView app.js

---
+++ js/view/selectDishView.js
@@ -0,0 +1,16 @@
+//Select dish view
+var SelectDishView = function (container, model) {
+
+	//this.numberOfGuests = container.find("#numberOfGuests");
+	var selectDishView = container.find("#selectDishView");
+	var testar = container.find("#testar");
+
+	testar.append("<div>hejsan</div>");
+
+	//var row = testar.find("#row");
+	//row.append("<div class='col-md-12'> <h1>HOMELETTE</h1> <i>From the best chefs in the world directly into your kitchen</i></div>");
+	//testar.append("<div class='col-md-12'> <h1>HOMELETTE</h1> <i>From the best chefs in the world directly into your kitchen</i></div>");
+	//testar.append("</div>");
+	testar.html("hej");
+
+}
