9c9fedf411a5cfca60bb715d841b30a73d9652b3
lambertsson <lambert002@gmail.com>
2015-02-10 15:11:31 +0100

    HomeView (utan bakgrund)

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -1,14 +1,8 @@
 var HomeView = function (container, model) {
 
-    var head = container.find("#headerDiv");
-    var side = container.find("#sideDiv");
-    var sideHead = container.find("#sHeadDiv");
-    var sideBody = container.find("#sBodyDiv");
-    var sideFooter = container.find("#sFooterDiv");
+    var createBtn = container.find("#create");
 
-    head.find("#header").html("HOMELETTE");
-    head.find("#subheader").html("From the best chefs in the world directly into your kitchen")
-    sideHead.html("A Home Dinner Service")
-    sideBody.html("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulllamaco laboris nisi ut aliquip ex ea commando consequat.")
-    sideFooter.html("start quickly<br><button id='addDish' class='btn'><span class='glyphicon glyphicon-plus'></span></button>")
+    createBtn.click(function () {
+        displayView("selectDishView")
+    })
 }
