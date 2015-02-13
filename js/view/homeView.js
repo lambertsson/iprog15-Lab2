824d7126d45948f42caa6797492faf23269e2d1a
lambertsson <lambert002@gmail.com>
2015-02-13 21:49:49 +0100

    Cleaned and structured html

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -5,6 +5,7 @@ var HomeView = function (container, model) {
     createBtn.click(function () {
         // Remove the image set on background when navigating away from home view.
         $('.container').css('background-image', 'none');
+        $('.header-bar').css('background-color', '#EEEEEE');
 
         // Update model and then only display selectDishView.
         model.update()
