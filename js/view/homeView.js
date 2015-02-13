70950ec1d362431ea034a678b110108302ce87f3
lambertsson <lambert002@gmail.com>
2015-02-13 21:29:04 +0100

    [DONE] HomeView
    Fixat formatering, städat upp, fungerar bra med olika fönsterstorlekar,
    har nu bakgrundsbild också. Kan anses som final.

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -3,6 +3,10 @@ var HomeView = function (container, model) {
     var createBtn = container.find("#create");
 
     createBtn.click(function () {
+        // Remove the image set on background when navigating away from home view.
+        $('.container').css('background-image', 'none');
+
+        // Update model and then only display selectDishView.
         model.update()
         displayView("selectDishView")
     })
