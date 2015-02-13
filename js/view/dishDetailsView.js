6b2f1016bcf37e2a2b42e74165623addebb43bd1
Caroline Arkenson <arkenson@kth.se>
2015-02-13 14:09:24 +0100

    stuff

--- js/view/dishDetailsView.js
+++ js/view/dishDetailsView.js
@@ -14,5 +14,20 @@ var DishDetailsView = function (container, model) {
 
     this.update = function () {
         this.dishDetailsView = container.find("#dishDetailsView");
+
+
+       	var mydishes = model.getAllDishes().prevObject;
+
+	    for (var i = 0; i < mydishes.length; i++) {
+	    	console.log("dishes");
+	        console.log(mydishes[i]);
+	        if(mydishes[i].name == "Meat balls"){
+	        console.log("meat balls mm");
+		
+		$("#dishDescription").after("<div><p>"+mydishes[i].title+"</p><img src='images/"+mydishes[i].image+"><br><br><p>"+mydishes[i].description+"</p></div>");
+		}
+
+
+	    }
     }
 }
