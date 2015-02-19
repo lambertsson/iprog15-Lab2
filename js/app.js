c9727f0ed59d3b542d149e37e3c19e2a64e35d2f
Caroline Arkenson <arkenson@kth.se>
2015-02-19 13:37:07 +0100

    initializing all the controllers

--- js/app.js
+++ js/app.js
@@ -4,15 +4,24 @@ $(function() {
 	
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"), model);
+
 	var selectDishView = new SelectDishView($("#selectDishView"),model);
+	var selectDishViewController = new SelectDishViewController(selectDishView, model);
 
 	var homeView = new HomeView($("#homeView"), model);
 	var homeViewController = new HomeViewController(homeView,model);
 
 	var dinnerOverviewView = new DinnerOverviewView($('#dinnerOverviewView'), model);
+	var dinnerOverviewViewController = new DinnerOverviewViewcontroller(dinnerOverviewView, model);
+
 	var dinnerPreparationView = new DinnerPreparationView($('#dinnerPreparationView'), model);
+	var dinnerPreparationViewController = new DinnerPreparationViewController(dinnerPreparationView, model);
+
 	var dishDetailsView = new DishDetailsView($('#dishDetailsView'), model);
+	var dishDetailsViewController = new DishDetailsViewController(dishDetailsView, model);
+
 	var sideMenuView = new SideMenuView($('#sideMenuView'), model);
+	var sideMenuViewController = new SideMenuViewController(sideMenuView, model);
 
 	displayView("homeView");
 });
