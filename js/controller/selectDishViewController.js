806f463c8210b1d49c7835aac570f2ba20581889
lambertsson <lambert002@gmail.com>
2015-02-19 15:56:11 +0100

    :ferris_wheel:

--- js/controller/selectDishViewController.js
+++ js/controller/selectDishViewController.js
@@ -3,7 +3,6 @@ var SelectDishViewController = function (view, model) {
     view.container.find("#dishDiv1").click(function () {
         if ($('#dishDiv1').data('id') > 0) {
             selectedDishID = $('#dishDiv1').data("id");
-            selectedDishID = 0;
             displayView("dishDetailsView");
             model.update();
         }
@@ -11,7 +10,6 @@ var SelectDishViewController = function (view, model) {
     view.container.find("#dishDiv2").click(function () {
         if ($('#dishDiv2').data('id') > 0) {
             selectedDishID = $('#dishDiv2').data("id");
-            selectedDishID = 0;
             displayView("dishDetailsView");
             model.update();
         }
@@ -19,7 +17,6 @@ var SelectDishViewController = function (view, model) {
     view.container.find("#dishDiv3").click(function () {
         if ($('#dishDiv3').data('id') > 0) {
             selectedDishID = $('#dishDiv3').data("id");
-            selectedDishID = 0;
             displayView("dishDetailsView");
             model.update();
         }
@@ -27,7 +24,6 @@ var SelectDishViewController = function (view, model) {
     view.container.find("#dishDiv4").click(function () {
         if ($('#dishDiv4').data('id') > 0) {
             selectedDishID = $('#dishDiv4').data("id");
-            selectedDishID = 0;
             displayView("dishDetailsView");
             model.update();
         }
@@ -35,7 +31,6 @@ var SelectDishViewController = function (view, model) {
     view.container.find("#dishDiv5").click(function () {
         if ($('#dishDiv5').data('id') > 0) {
             selectedDishID = $('#dishDiv5').data("id");
-            selectedDishID = 0;
             displayView("dishDetailsView");
             model.update();
         }
@@ -43,7 +38,6 @@ var SelectDishViewController = function (view, model) {
     view.container.find("#dishDiv6").click(function () {
         if ($('#dishDiv6').data('id') > 0) {
             selectedDishID = $('#dishDiv6').data("id");
-            selectedDishID = 0;
             displayView("dishDetailsView");
             model.update();
         }
@@ -51,7 +45,6 @@ var SelectDishViewController = function (view, model) {
     view.container.find("#dishDiv7").click(function () {
         if ($('#dishDiv7').data('id') > 0) {
             selectedDishID = $('#dishDiv7').data("id");
-            selectedDishID = 0;
             displayView("dishDetailsView");
             model.update();
         }
@@ -59,7 +52,6 @@ var SelectDishViewController = function (view, model) {
     view.container.find("#dishDiv8").click(function () {
         if ($('#dishDiv8').data('id') > 0) {
             selectedDishID = $('#dishDiv8').data("id");
-            selectedDishID = 0;
             displayView("dishDetailsView");
             model.update();
         }
@@ -67,7 +59,6 @@ var SelectDishViewController = function (view, model) {
     view.container.find("#dishDiv9").click(function () {
         if ($('#dishDiv9').data('id') > 0) {
             selectedDishID = $('#dishDiv9').data("id");
-            selectedDishID = 0;
             displayView("dishDetailsView");
             model.update();
         }
@@ -75,7 +66,6 @@ var SelectDishViewController = function (view, model) {
     view.container.find("#dishDiv10").click(function () {
         if ($('#dishDiv10').data('id') > 0) {
             selectedDishID = $('#dishDiv10').data("id");
-            selectedDishID = 0;
             displayView("dishDetailsView");
             model.update();
         }
