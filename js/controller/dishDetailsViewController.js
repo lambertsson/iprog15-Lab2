f437215b1f8464b8911065912a6f28509babe314
Caroline Arkenson <arkenson@kth.se>
2015-02-19 15:59:23 +0100

    :sake:

--- js/controller/dishDetailsViewController.js
+++ js/controller/dishDetailsViewController.js
@@ -3,12 +3,15 @@ var DishDetailsViewController = function (view, model) {
 
     view.backBtn.click(function () {
         displayView("selectDishView")
+        selectedDishID = 0;
         model.update()
+
     })
 
     view.confirmDishBtn.click(function(){
-    	console.log(selectedDishID);
+    	console.log("försöker confirma dish", selectedDishID);
     	model.addDishToMenu(selectedDishID);
+    	selectedDishID = 0;
     	model.update()    	
     })
 }
